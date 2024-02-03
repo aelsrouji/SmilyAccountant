d8a021b225656b62652e0200702174b83b32f7f0
Ayman <ayman4001@hotmail.com>
2024-02-03 13:44:48 -0500

    Employees Controller

---
+++ Areas/Finance/Controllers/EmployeesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SmilyAccountant.Areas.Finance.Models;
+using SmilyAccountant.Data;
+
+namespace SmilyAccountant.Areas.Finance.Controllers
+{
+    [Area("Finance")]
+    public class EmployeesController : Controller
+    {
+        private readonly SmilyAccountantContext _context;
+
+        public EmployeesController(SmilyAccountantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Finance/Employees
+        public async Task<IActionResult> Index()
+        {
+              return _context.Employees != null ? 
+                          View(await _context.Employees.ToListAsync()) :
+                          Problem("Entity set 'SmilyAccountantContext.Employees'  is null.");
+        }
+
+        // GET: Finance/Employees/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Finance/Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Finance/Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EmployeeNumber,FirstName,MiddleName,LastName,JobTitle,Initials,Gender,CompanyEmail,CompanyPhone,Extension,PrivateEmail,PrivatePhoneNumber,Address,Address2,City,CountryId,Region,PostalCode,EmployementDate,EmploymenetStatus,InactiveDate,TerminationDate,BirthDate,SSN,BankBranchNumber,BankAccountNumber,IBAN,SwiftCode")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employee.Id = Guid.NewGuid();
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Finance/Employees/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Finance/Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,EmployeeNumber,FirstName,MiddleName,LastName,JobTitle,Initials,Gender,CompanyEmail,CompanyPhone,Extension,PrivateEmail,PrivatePhoneNumber,Address,Address2,City,CountryId,Region,PostalCode,EmployementDate,EmploymenetStatus,InactiveDate,TerminationDate,BirthDate,SSN,BankBranchNumber,BankAccountNumber,IBAN,SwiftCode")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Finance/Employees/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Finance/Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Employees == null)
+            {
+                return Problem("Entity set 'SmilyAccountantContext.Employees'  is null.");
+            }
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(Guid id)
+        {
+          return (_context.Employees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
