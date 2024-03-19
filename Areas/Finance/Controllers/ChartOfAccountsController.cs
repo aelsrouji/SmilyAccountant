91c57be89c311f8a45b7647ce0e0a0bb4f18a11e
Ayman <ayman4001@hotmail.com>
2024-03-19 00:25:37 -0400

    Adding details to chart of account page Account no, type, category and sub category

--- Areas/Finance/Controllers/ChartOfAccountsController.cs
+++ Areas/Finance/Controllers/ChartOfAccountsController.cs
@@ -23,140 +23,143 @@ namespace SmilyAccountant.Areas.Finance.Controllers
         // GET: Finance/ChartOfAccounts
         public async Task<IActionResult> Index()
         {
-            var smilyAccountantContext = _context.ChartOfAccounts.Include(c => c.GLAccountCard);
+            var smilyAccountantContext = _context.ChartOfAccounts.Include(c => c.GLAccountCard)
+                .Include(t => t.GLAccountCard.AccountType)
+                .Include(ac => ac.GLAccountCard.AccountCategory)
+                .Include(asc => asc.GLAccountCard.AccountSubCategory);
             return View(await smilyAccountantContext.ToListAsync());
         }
 
         // GET: Finance/ChartOfAccounts/Details/5
-        public async Task<IActionResult> Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Details(Guid? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var chartOfAccount = await _context.ChartOfAccounts
-                .Include(c => c.GLAccountCard)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (chartOfAccount == null)
-            {
-                return NotFound();
-            }
+        //    var chartOfAccount = await _context.ChartOfAccounts
+        //        .Include(c => c.GLAccountCard)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (chartOfAccount == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(chartOfAccount);
-        }
+        //    return View(chartOfAccount);
+        //}
 
         // GET: Finance/ChartOfAccounts/Create
-        public IActionResult Create()
-        {
-            ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id");
-            return View();
-        }
+        //public IActionResult Create()
+        //{
+        //    ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id");
+        //    return View();
+        //}
 
         // POST: Finance/ChartOfAccounts/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,GLAccountCardId,NetChange,NetBalanace")] ChartOfAccount chartOfAccount)
-        {
-            if (ModelState.IsValid)
-            {
-                chartOfAccount.Id = Guid.NewGuid();
-                _context.Add(chartOfAccount);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id", chartOfAccount.GLAccountCardId);
-            return View(chartOfAccount);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,GLAccountCardId,NetChange,NetBalanace")] ChartOfAccount chartOfAccount)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        chartOfAccount.Id = Guid.NewGuid();
+        //        _context.Add(chartOfAccount);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id", chartOfAccount.GLAccountCardId);
+        //    return View(chartOfAccount);
+        //}
 
         // GET: Finance/ChartOfAccounts/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(Guid? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var chartOfAccount = await _context.ChartOfAccounts.FindAsync(id);
-            if (chartOfAccount == null)
-            {
-                return NotFound();
-            }
-            ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id", chartOfAccount.GLAccountCardId);
-            return View(chartOfAccount);
-        }
+        //    var chartOfAccount = await _context.ChartOfAccounts.FindAsync(id);
+        //    if (chartOfAccount == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id", chartOfAccount.GLAccountCardId);
+        //    return View(chartOfAccount);
+        //}
 
         // POST: Finance/ChartOfAccounts/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,GLAccountCardId,NetChange,NetBalanace")] ChartOfAccount chartOfAccount)
-        {
-            if (id != chartOfAccount.Id)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(Guid id, [Bind("Id,GLAccountCardId,NetChange,NetBalanace")] ChartOfAccount chartOfAccount)
+        //{
+        //    if (id != chartOfAccount.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(chartOfAccount);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ChartOfAccountExists(chartOfAccount.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id", chartOfAccount.GLAccountCardId);
-            return View(chartOfAccount);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(chartOfAccount);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!ChartOfAccountExists(chartOfAccount.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["GLAccountCardId"] = new SelectList(_context.GLAccountCards, "Id", "Id", chartOfAccount.GLAccountCardId);
+        //    return View(chartOfAccount);
+        //}
 
         // GET: Finance/ChartOfAccounts/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(Guid? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var chartOfAccount = await _context.ChartOfAccounts
-                .Include(c => c.GLAccountCard)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (chartOfAccount == null)
-            {
-                return NotFound();
-            }
+        //    var chartOfAccount = await _context.ChartOfAccounts
+        //        .Include(c => c.GLAccountCard)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (chartOfAccount == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(chartOfAccount);
-        }
+        //    return View(chartOfAccount);
+        //}
 
         // POST: Finance/ChartOfAccounts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var chartOfAccount = await _context.ChartOfAccounts.FindAsync(id);
-            if (chartOfAccount != null)
-            {
-                _context.ChartOfAccounts.Remove(chartOfAccount);
-            }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(Guid id)
+        //{
+        //    var chartOfAccount = await _context.ChartOfAccounts.FindAsync(id);
+        //    if (chartOfAccount != null)
+        //    {
+        //        _context.ChartOfAccounts.Remove(chartOfAccount);
+        //    }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool ChartOfAccountExists(Guid id)
         {
