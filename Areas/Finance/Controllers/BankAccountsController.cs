e1e22b8c0a8bec20d65f9159aa8d76e7a89d75c7
Ayman <ayman4001@hotmail.com>
2024-04-29 19:32:18 -0400

    Adding bank accounts controller and views - Finanace administration

---
+++ Areas/Finance/Controllers/BankAccountsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SmilyAccountant.Areas.Finance.Models;
+using SmilyAccountant.Data;
+
+namespace SmilyAccountant.Areas.Finance.Controllers
+{
+    [Area("Finance")]
+    public class BankAccountsController : Controller
+    {
+        private readonly SmilyAccountantContext _context;
+
+        public BankAccountsController(SmilyAccountantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Finance/BankAccounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BankAccounts.ToListAsync());
+        }
+
+        // GET: Finance/BankAccounts/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(bankAccount);
+        }
+
+        // GET: Finance/BankAccounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Finance/BankAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,No,BranchNumber,BankAccountNumber,Balance,Blocked")] BankAccount bankAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                bankAccount.Id = Guid.NewGuid();
+                _context.Add(bankAccount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankAccount);
+        }
+
+        // GET: Finance/BankAccounts/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccounts.FindAsync(id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+            return View(bankAccount);
+        }
+
+        // POST: Finance/BankAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,No,BranchNumber,BankAccountNumber,Balance,Blocked")] BankAccount bankAccount)
+        {
+            if (id != bankAccount.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bankAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BankAccountExists(bankAccount.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankAccount);
+        }
+
+        // GET: Finance/BankAccounts/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bankAccount = await _context.BankAccounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(bankAccount);
+        }
+
+        // POST: Finance/BankAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var bankAccount = await _context.BankAccounts.FindAsync(id);
+            if (bankAccount != null)
+            {
+                _context.BankAccounts.Remove(bankAccount);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BankAccountExists(Guid id)
+        {
+            return _context.BankAccounts.Any(e => e.Id == id);
+        }
+    }
+}
